54a1b644b939f9ab619d2a6d259c68566979d92a
 mac <kksanaa@mail.ru>
2022-10-13 14:38:41 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+void GetResult(int num1, int num2)
+ {
+   int result =  num1;
+   for (int i = 1; i < num2; i++)
+   {
+    result = result * num1;
+   }
+    Console.WriteLine($"{num1}, {num2} -> {result}");
+ }
+ GetResult(2,4);
